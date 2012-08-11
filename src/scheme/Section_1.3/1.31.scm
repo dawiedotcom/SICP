61d7a86cc4c32db3723bd10563bc6429dfbdd9d5
David de Klerk <dawiedotcom@gmail.com>
2012-08-11 15:07:18 +0200

    Add a load statment and some comments to Exercise 1.31.

--- src/scheme/Section_1.3/1.31.scm
+++ src/scheme/Section_1.3/1.31.scm
@@ -2,7 +2,11 @@
 ;	Dawie de Klerk
 ;	2012-08-10
 
+(load "../utils.scm")
+
 (define (product term a next b)
+  ;; Almost exactly the same as sum, but with
+  ;; * instead of +
   (define (iter a result)
 	(if (> a b) result
 	    (iter (next a) (* result (term a)))))
@@ -12,22 +16,25 @@
 (define (inc x) (+ 1 x))
 
 (define (factorial n)
+  ;; Factorial is the product 1.2.3...n
   (product identity 1 inc n))
 
-
+;;; Test factorial with some known input:
 (println "2! =" (factorial 2))
 (println "3! =" (factorial 3))
 (println "4! =" (factorial 4))
 (println "5! =" (factorial 5))
 
 (define (pi/4 m)
+  ;; Calculate an approximation to one fourth of pi 
+  ;; using John Willis' formula.
   (define (pi-term n)
-	; Group 4 numbers into a term. For n=1: (2.4)/(3.3), etc
+	; Group 4 numbers into a term, i.e. for n=1: (2.4)/(3.3), etc
 	(define n2 (* 2 n))
 	(exact->inexact (* n2 (+ 2 n2) (/ 1 (square (+ n2 1))))))
   (product pi-term 1 inc m))
-;(define pi (* (pi/4 100) 4))
 
+;;; Print a few approximations to pi
 (println "pi =" (* (pi/4 10) 4) " with 10 iterations")
 (println "pi =" (* (pi/4 100) 4) " with 100 iterations")
 (println "pi =" (* (pi/4 1000) 4) " with 1000 iterations")
