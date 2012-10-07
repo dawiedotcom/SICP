357d0db3599876a310c039c38a4eb4c9bb8da005
David de Klerk <dawiedotcom@gmail.com>
2012-10-07 22:45:09 +0200

    Add solution for 3.19, updated 3.18.

--- src/scheme/Section_3.3/3.18.scm
+++ src/scheme/Section_3.3/3.18.scm
@@ -5,7 +5,8 @@
 (load "../utils.scm")
 
 (define (cyclic? x)
-  ;; Counts the number of unique pairs in x.
+  ;; Returns true if x contains any cycles that
+  ;; would cause an infinite loop by cdr-ing through x.
   (let ((visited '()))
     (define (iter x)
       (cond ((null? (cdr x)) #f)
@@ -18,12 +19,24 @@
 
 
 (define (do-examples)
+  ;; A normal list with three symbols
   (print-eval (cyclic? '(a b c)))
+  ;; The first pair points to the second and third
   (define x '(a b))
   (print-eval (cyclic? (cons x (cdr x))))
-
+  ;; Each pair points to the next twice
   (define y '(a))
   (define z (cons y y))
+  ;; A cyclic 2 element list
   (print-eval (cyclic? (cons z z)))
   (set-cdr! (cdr x) x)
-  (print-eval (cyclic? x)))
+  (print-eval (cyclic? x))
+  ;; A cyclic 1 element list
+  (define w (cons 'a 'b))
+  (set-cdr! w w)
+  (print-eval (cyclic? w))
+  ;; A cyclic 2 element list where the 
+  ;; last element points to itself
+  (define s (list 'a 'b))
+  (set-cdr! (cdr s) (cdr s))
+  (print-eval (cyclic? s)))
