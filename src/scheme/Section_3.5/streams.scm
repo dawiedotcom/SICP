c1fdf73a662afaa5a3a1451c4db5baaf120398c7
David de Klerk <dawiedotcom@gmail.com>
2013-04-07 10:30:39 +0200

    Add a solution for Exercise 3.59.

--- src/scheme/Section_3.5/streams.scm
+++ src/scheme/Section_3.5/streams.scm
@@ -182,3 +182,17 @@
                                 (merge (scale-stream S 3)
                                        (scale-stream S 5)))))
 
+;;; Exercise 3.59
+
+(define (integrate-series s)
+  (stream-map / s integers))
+
+(define exp-series
+  (cons-stream 1 (integrate-series exp-series)))
+
+(define cosine-series 
+  (cons-stream 1 (stream-map (lambda (x) (- x))
+                             (integrate-series sine-series))))
+
+(define sine-series 
+  (cons-stream 0 (integrate-series cosine-series)))
