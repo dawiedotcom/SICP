4b7277cce88ad64990c815c15fc2674574a223c7
David de Klerk <dawiedotcom@gmail.com>
2013-04-21 18:15:35 +0200

    Add a solution for Exercise 3.80

--- src/scheme/Section_3.5/streams.scm
+++ src/scheme/Section_3.5/streams.scm
@@ -493,3 +493,16 @@
                      1
                      0.001)
       1000)))
+
+;;; Exercise 3.80
+
+(define (RLC r l c dt)
+  (define (result vc0 il0)
+    (define vc (integral (delay dvc) vc0 dt))
+    (define il (integral (delay dil) il0 dt))
+    (define dil (add-streams (scale-stream vc (/ 1 l))
+                             (scale-stream il (- (/ r l)))))
+    (define dvc (scale-stream il (- (/ 1 c))))
+    (cons vc il))
+  result)
+  
